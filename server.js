1141dc9a89ae376720274008d44578e3aa3aebb5
John Phillips <jphillips37@gmail.com>
2019-02-14 20:13:28 -0500

    cleaned up files

--- server.js
+++ server.js
@@ -4,21 +4,17 @@ var PORT = process.env.PORT || 3000;
 
 var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Set Handlebars.
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/catsController.js");
+var routes = require("./controllers/Controller.js");
 
 app.use(routes);
 
