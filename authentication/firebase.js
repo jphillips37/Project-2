627917d1862d3f65be07dc3097834ee3c9503e7a
Drew Lauck <drewlauck92@gmail.com>
2019-02-18 13:55:57 -0500

    adding firebase links

---
+++ authentication/firebase.js
@@ -0,0 +1,15 @@
+var config = {
+    apiKey: "AIzaSyCh7cia3s-U2b6gK11yo0U7URcEB0HPYJ0",
+    authDomain: "project-2-e5bca.firebaseapp.com",
+    databaseURL: "https://project-2-e5bca.firebaseio.com",
+    projectId: "project-2-e5bca",
+    storageBucket: "",
+    messagingSenderId: "564080125605"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  
+
+  
