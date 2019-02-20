880bc4dbac81c3d7e119dbd16c043d21c9fdb11f
John Phillips <jphillips37@gmail.com>
2019-02-20 18:21:43 -0500

    updates to db model and html routes

--- models/posts.js
+++ models/posts.js
@@ -14,6 +14,13 @@ module.exports = function(sequelize, DataTypes) {
                 len: [1]
             }
         },
+        post_title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
         post_body: {
             type: DataTypes.TEXT,
             allowNull: false,
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,20 +1,24 @@
-// *********************************************************************************
-// html-routes.js - this file offers a set of routes for sending users to the various html pages
-// *********************************************************************************
-
-// Dependencies
-// =============================================================
 var path = require("path");
-
-// Routes
-// =============================================================
+var db = require("../models/index.js");
 module.exports = function(app) {
 
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/blog.html"));
+    var object = {};
+    db.Post.findAll({
+      order:[
+        ["createdAt", "DESC"]
+      ],
+      limit: 2
+    }).then(function(results){
+      object.title = results[0].post_title;
+      object.body = results[0].post_body;
+      console.log(object);
+      res.render("index", object);
+    })
+
   });
 
   app.get("/cms", function(req, res) {
