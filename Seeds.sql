c2e83776ca6be152380d25e99eca8cda6e08ae17
John Phillips <jphillips37@gmail.com>
2019-02-24 16:50:41 -0500

    html routes and api routes updated

---
+++ Seeds.sql
@@ -0,0 +1,32 @@
+use ea8t2ptee87rhxc4;
+
+insert into Users (user_name, createdAt, updatedAt) values ('User 1', current_timestamp(), current_timestamp());
+insert into Users (user_name, createdAt, updatedAt) values ('User 2', current_timestamp(), current_timestamp());
+insert into Users (user_name, createdAt, updatedAt) values ('User 3', current_timestamp(), current_timestamp());
+insert into Users (user_name, createdAt, updatedAt) values ('User 4', current_timestamp(), current_timestamp());
+insert into Users (user_name, createdAt, updatedAt) values ('User 5', current_timestamp(), current_timestamp());
+insert into Users (user_name, createdAt, updatedAt) values ('User 6', current_timestamp(), current_timestamp());
+insert into Users (user_name, createdAt, updatedAt) values ('User 7', current_timestamp(), current_timestamp());
+insert into Users (user_name, createdAt, updatedAt) values ('User 8', current_timestamp(), current_timestamp());
+insert into Users (user_name, createdAt, updatedAt) values ('User 9', current_timestamp(), current_timestamp());
+insert into Users (user_name, createdAt, updatedAt) values ('User 10', current_timestamp(), current_timestamp());
+insert into Users (user_name, createdAt, updatedAt) values ('User 11', current_timestamp(), current_timestamp());
+insert into Users (user_name, createdAt, updatedAt) values ('User 12', current_timestamp(), current_timestamp());
+insert into Users (user_name, createdAt, updatedAt) values ('User 13', current_timestamp(), current_timestamp());
+insert into Users (user_name, createdAt, updatedAt) values ('User 14', current_timestamp(), current_timestamp());
+insert into Users (user_name, createdAt, updatedAt) values ('User 15', current_timestamp(), current_timestamp());
+insert into Users (user_name, createdAt, updatedAt) values ('User 16', current_timestamp(), current_timestamp());
+insert into Users (user_name, createdAt, updatedAt) values ('User 17', current_timestamp(), current_timestamp());
+insert into Users (user_name, createdAt, updatedAt) values ('User 18', current_timestamp(), current_timestamp());
+insert into Users (user_name, createdAt, updatedAt) values ('User 19', current_timestamp(), current_timestamp());
+insert into Users (user_name, createdAt, updatedAt) values ('User 20', current_timestamp(), current_timestamp());
+insert into Users (user_name, createdAt, updatedAt) values ('User 21', current_timestamp(), current_timestamp());
+
+insert into Posts (region, city, post_title, post_body, post_points, createdAt, updatedAt, UserId) values ('South West','Houston','Title 1','Post Text 1', 0,current_timestamp(), current_timestamp(), 2);
+insert into Posts (region, city, post_title, post_body, post_points, createdAt, updatedAt, UserId) values ('East Coast','Miami','Title 2','Post Text 2', 0,current_timestamp(), current_timestamp(), 6);
+insert into Posts (region, city, post_title, post_body, post_points, createdAt, updatedAt, UserId) values ('West Coast','San Diego','Title 3','Post Text 3', 0,current_timestamp(), current_timestamp(), 14);
+insert into Posts (region, city, post_title, post_body, post_points, createdAt, updatedAt, UserId) values ('Gulf Coast','Naples','Title 4','Post Text 4', 0,current_timestamp(), current_timestamp(), 11);
+insert into Posts (region, city, post_title, post_body, post_points, createdAt, updatedAt, UserId) values ('West Coast','San Fransisco','Title 5','Post Text 5', 0,current_timestamp(), current_timestamp(), 19);
+insert into Posts (region, city, post_title, post_body, post_points, createdAt, updatedAt, UserId) values ('Hawaii','Honalulu','Title 6','Post Text 6', 0,current_timestamp(), current_timestamp(), 15);
+
+select * from Posts;
