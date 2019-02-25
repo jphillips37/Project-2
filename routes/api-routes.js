70fb96f524ae2759ca470b3c3625a9182293354f
John Phillips <jphillips37@gmail.com>
2019-02-25 17:34:44 -0500

    updated routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -16,78 +16,24 @@ module.exports = function(app) {
         res.json(results);
       });
   });
-  // app.get("/posts", function(req, res) {
-  //   db.Post.findAll({})
-  //     .then(function(dbPost) {
-  //       res.json(dbPost);
-  //     });
-  // });
 
-  // app.get("/posts/:region", function(req, res) {
-  //   db.Post.findAll({
-  //     where: {
-  //       region: req.params.region
-  //     }
-  //   })
-  //     .then(function(dbPost) {
-  //       res.json(dbPost);
-  //     });
-  // });
+  app.get("/api/users", function(req, res){
+    db.User.findAll({
+      order:[
+        ["createdAt", "DESC"]
+      ]
+    }).then(function(results){
+      res.json(results);
+    })
+  });
 
-  // app.get("/posts/:id", function(req, res) {
-  //   db.Post.findOne({
-  //     where: {
-  //       id: req.params.id
-  //     }
-  //   })
-  //     .then(function(dbPost) {
-  //       res.json(dbPost);
-  //     });
-  // });
-
-    // app.get("/posts/:author", function(req, res) {
-    //     db.Users.findOne({
-    //       where: {
-    //         user_name: req.params.user_name
-    //       }
-    //     })
-    //       .then(function(dbPost) {
-    //         res.json(dbPost);
-    //       });
-    //   });
-
-  // app.post("/posts", function(req, res) {
-  //   console.log(req.body);
-  //   db.Post.create({
-  //     title: req.body.title,
-  //     body: req.body.body,
-  //     category: req.body.category
-  //   })
-  //     .then(function(dbPost) {
-  //       res.json(dbPost);
-  //     });
-  // });
-
-  // app.delete("/posts/:id", function(req, res) {
-  //   db.Post.destroy({
-  //     where: {
-  //       id: req.params.id
-  //     }
-  //   })
-  //     .then(function(dbPost) {
-  //       res.json(dbPost);
-  //     });
-  // });
-
-  // app.put("/posts", function(req, res) {
-  //   db.Post.update(req.body,
-  //     {
-  //       where: {
-  //         id: req.body.id
-  //       }
-  //     })
-  //     .then(function(dbPost) {
-  //       res.json(dbPost);
-  //     });
-  // });
+  app.get("/api/posts", function(req, res){
+    db.Post.findAll({
+      order:[
+        ["createdAt", "DESC"]
+      ]
+    }).then(function(results){
+      res.json(results);
+    });
+  });
 };
